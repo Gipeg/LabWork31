ab03b08a74b6aa0acd8a9db13fb70c2851f7d845
fisp <arenatkanovna@gmail.com>
2024-06-19 00:21:02 +0300

    Добавьте файлы проекта.

---
+++ Task2/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(LoginTextBox.Text) || string.IsNullOrEmpty(PrimaryPasswordBox.Password) || PrimaryPasswordBox.Password != ConfirmPasswordBox.Password)
+                MessageBox.Show("Некорректные данные", "Ошибка");
+            else
+                MessageBox.Show($"{LoginTextBox.Text}, вы зарегестрирован", "УРААААААААААА!!!!!!!!");
+        }
+    }
+}
